c8d7ea13f3d63f2671afcffe52787d878fc0fb54
Francis <148550609+Frxncz@users.noreply.github.com>
2025-12-10 10:32:31 +0800

    Added a simple UI design in the start screen

--- script/start_screen.gd
+++ script/start_screen.gd
@@ -13,3 +13,13 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/world.tscn")
+
+
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	pass # Replace with function body.
