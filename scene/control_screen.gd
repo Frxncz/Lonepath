b4b9435934e19a485fa6a3eec58be45dc702407e
Francis <148550609+Frxncz@users.noreply.github.com>
2025-12-13 19:39:30 +0800

    feat(ui): Added a control screen design and its functionality when clicked it

--- scene/control_screen.gd
+++ scene/control_screen.gd
@@ -11,15 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene/world.tscn")
-
-
-
-
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene/control_screen.tscn")
-
-
-func _on_exit_pressed() -> void:
-	get_tree().quit()
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/start_screen.tscn")
